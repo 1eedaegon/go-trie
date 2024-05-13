d65aa90778d23e02edeab3cd287e6d3bc99bbbe3
1eedaegon <d8726243@gmail.com>
2024-05-13 16:48:08 +0900

    chore: Fix comment for trier

--- trie.go
+++ trie.go
@@ -7,11 +7,10 @@ const (
 	TypeBitTrie     TrieType = 1
 )
 
-// Trier has 4 methods
+// Trie has 3 methods
 // 1. Get by key
 // 2. Put with key and value
 // 3. Delete by key
-// 4. Walk with walker function
 
 type Trier interface {
 	Get(key string) interface{}
